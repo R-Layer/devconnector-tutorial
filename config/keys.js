e9316fd2eb600d27f077477c1ffddbab0c1aa516
R-Layer <layer@europe.com>
2018-07-04 10:44:05 +0200

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGODB_URI: 'mongodb://layer:]breaD44[@ds125341.mlab.com:25341/devconnector-tutorial'
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const configVars = require("./config/keys");
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+mongoose
+  .connect(configVars.MONGODB_URI, {useNewUrlParser: true})
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello"));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
