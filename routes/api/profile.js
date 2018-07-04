0064237b969d49ebc9cc0214b22ee0fb2306fa9e
R-Layer <layer@europe.com>
2018-07-04 16:28:10 +0200

    Add profile model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,13 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
+const router = express.Router();
 //@route    GET api/profile/test
 //@desc     Tests profile route
 //@access   Public
-router.get('/test', (req, res) => res.status(200).json({msg: "Profile Works"}));
+router.get("/test", (req, res) =>
+  res.status(200).json({ msg: "Profile Works" })
+);
 
 module.exports = router;
