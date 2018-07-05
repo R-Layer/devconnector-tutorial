a32932427d3a9e37cee83a7d48310f28c3ad978a
R-Layer <layer@europe.com>
2018-07-05 11:13:18 +0200

    Add post model

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create a schema
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
+});
+
+const Post = mongoose.model("post", postSchema);
+
+module.exports = Post;
