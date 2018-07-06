314dbe0c71f210ac93edafb8e385ff328491c901
R-Layer <layer@europe.com>
2018-07-06 10:20:58 +0200

    Add actions - closed test workflow

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { TEST_DISPATCH } from "../actions/types";
+
 const intialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +7,8 @@ const intialState = {
 
 export default function(state = intialState, action) {
   switch (action.type) {
+    case TEST_DISPATCH:
+      return { ...state, user: action.payload };
     default:
       return state;
   }
