000a2130a59eecebd1642f0166adb03dd06e9066
R-Layer <layer@europe.com>
2018-07-06 16:31:49 +0200

    Add profile redux stuff

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,11 @@ import Landing from "./components/layout/Landing";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+
 import store from "./store";
 import "./App.css";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 // check for token
 if (localStorage.jwtToken) {
@@ -30,7 +33,7 @@ if (localStorage.jwtToken) {
     // Logout the user
     store.dispatch(logoutUser());
     // Clear crrent Profile
-    // ...
+    store.dispatch(clearCurrentProfile());
     // Redirect to logi
     window.location.href = "/login";
   }
@@ -47,6 +50,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
