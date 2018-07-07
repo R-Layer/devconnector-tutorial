1a5decb76e664d51ee861179fe80acf0d86005b8
R-Layer <layer@europe.com>
2018-07-07 12:13:44 +0200

    Add delete account functionality and setup edit routes

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER
 } from "./types";
 
 // Get current profile
@@ -39,6 +40,21 @@ export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
   };
+};
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This CANNOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res => dispatch({ type: SET_CURRENT_USER, payload: {} }))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
 
 // Clear profile
