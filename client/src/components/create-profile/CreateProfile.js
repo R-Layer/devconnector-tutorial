fed50b6d807ba3aaa5605344d34e373c099de8a1
R-Layer <layer@europe.com>
2018-07-07 11:53:21 +0200

    Implemented profile creation actions

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
+
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
+
+import { createProfile } from "../../actions/profileActions";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -30,6 +34,14 @@ class CreateProfile extends Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
   onChange = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -39,7 +51,23 @@ class CreateProfile extends Component {
   onSubmit = e => {
     e.preventDefault();
 
-    console.log("submit");
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    };
+
+    this.props.createProfile(profileData, this.props.history);
   };
 
   render() {
@@ -123,7 +151,7 @@ class CreateProfile extends Component {
                   info="A unique handle for your profile URL. Your full name, company name, nickname, etc"
                 />
                 <SelectListGroup
-                  placeholder="Status"
+                  placeholder="* Status"
                   name="status"
                   value={this.state.status}
                   onChange={this.onChange}
@@ -156,7 +184,7 @@ class CreateProfile extends Component {
                   info="City or city & state suggested(e.g. Boston, MA)"
                 />
                 <TextFieldGroup
-                  placeholder="Skills"
+                  placeholder="* Skills"
                   name="skills"
                   value={this.state.skills}
                   onChange={this.onChange}
@@ -182,6 +210,7 @@ class CreateProfile extends Component {
 
                 <div className="mb-3">
                   <button
+                    type="button"
                     onClick={() =>
                       this.setState(prevState => ({
                         displaySocialInputs: !prevState.displaySocialInputs
@@ -218,4 +247,7 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(
+  mapStateToProps,
+  { createProfile }
+)(withRouter(CreateProfile));
