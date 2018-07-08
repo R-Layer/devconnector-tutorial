1c843db09a469eabbe0ed5a334bc4f97057a3812
R-Layer <layer@europe.com>
2018-07-08 10:49:39 +0200

    Setup post redux - Post Component

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 import store from "./store";
 import "./App.css";
@@ -93,6 +94,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
